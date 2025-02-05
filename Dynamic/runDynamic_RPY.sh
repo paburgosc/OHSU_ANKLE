9a91eae0d8b5928a3e2e170cc8799a0728fe173a
paburgosc <130399228+paburgosc@users.noreply.github.com>
2025-02-05 13:04:05 -0800

    updated sh

--- Dynamic/runDynamic_RPY.sh
+++ Dynamic/runDynamic_RPY.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /home/pi/OHSU_ANKLE/Dynamic
+git pull
 lxterminal --command "sudo nice -n -20 /home/pi/.pyenv/versions/3.6.6/bin/python /home/pi/OHSU_ANKLE/Dynamic/main1force.py" &
 lxterminal --command "sudo nice -n -20 /home/pi/.pyenv/versions/3.6.6/bin/python /home/pi/OHSU_ANKLE/Dynamic/readforcecallable.py"
--- Kinematic/runKinematicSENSOR_RPY.sh
+++ Kinematic/runKinematicSENSOR_RPY.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /home/pi/OHSU_ANKLE/Kinematic
+git pull
 lxterminal --command "sudo nice -n -20 /home/pi/.pyenv/versions/3.6.6/bin/python /home/pi/OHSU_ANKLE/Kinematic/main1.py" &
 lxterminal --command "sudo nice -n -20 /home/pi/.pyenv/versions/3.6.6/bin/python /home/pi/OHSU_ANKLE/Kinematic/readeulercallable.py"
