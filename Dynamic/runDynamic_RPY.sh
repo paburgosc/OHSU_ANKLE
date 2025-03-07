672d44c93b068c0371423a71c8fb46466ea770e4
paburgosc <130399228+paburgosc@users.noreply.github.com>
2025-03-07 13:16:48 -0800

    Update runDynamic_RPY.sh

--- Dynamic/runDynamic_RPY.sh
+++ Dynamic/runDynamic_RPY.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd /home/pi/OHSU_ANKLE
+sudo fetch origin
+sudo git reset --hard origin/main
 sudo git pull
 cd /home/pi/OHSU_ANKLE/Dynamic
 lxterminal --command "sudo nice -n -20 /home/pi/.pyenv/versions/3.6.6/bin/python /home/pi/OHSU_ANKLE/Dynamic/main1force.py" &
